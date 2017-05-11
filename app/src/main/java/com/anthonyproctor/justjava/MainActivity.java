3e1eb96b2855b152718e8958bdb4f828351bbb70
Anthony <anthonyproctor@gmail.com>
2017-05-10 21:19:25 -0600

    Fix: Fixed the submitOrder Method.
    This fix uses the global variable quantity and only updates the display price now.

--- app/src/main/java/com/anthonyproctor/justjava/MainActivity.java
+++ app/src/main/java/com/anthonyproctor/justjava/MainActivity.java
@@ -11,7 +11,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 1;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
